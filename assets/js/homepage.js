e9a882f0c68e6905f5acb142db8f47bcf39fa818
gtankha <46304828+gtankha@users.noreply.github.com>
2020-08-25 18:30:32 -0700

    featurelanguages functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,22 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+ 
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+
+}
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -81,5 +96,18 @@ for (var i = 0; i < repos.length; i++) {
     repoContainerEl.appendChild(repoEl);
   }
   };
+  var buttonClickHandler =  function (event) {
+
+    var language  = event.target.getAttribute("data-language")
+       
+    if (language) {
+      getFeaturedRepos(language);
+    // clear old content
+    repoContainerEl.textContent = "";
+   
+    }
+
+  }
 
   userFormEl.addEventListener("submit", formSubmitHandler);
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
